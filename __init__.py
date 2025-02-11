dcd0cadb8c242855b37eb883ccfec062bc2d962a
洋輝 大橋 <hiro0084169@outlook.jp>
2025-02-11 16:50:02 +0900

    03_02

---
+++ __init__.py
@@ -0,0 +1,65 @@
+import bpy
+
+# モジュールのインポート
+from .stretch_vertex import MYADDON_OT_stretch_vertex
+from .ico_sphere import MYADDON_OT_create_ico_sphere
+from .collider import MYADDON_OT_add_collider
+from .collider import OBJECT_PT_collider
+from .collider import DrawCollider
+from .file import MYADDON_OT_add_filename
+from .file import OBJECT_PT_file_name
+from .scene import MYADDON_OT_export_scene
+from .my_menu import TOPBAR_MT_my_menu
+
+# ブレンダーに登録するアドオン情報
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "Hiroki Ohashi",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
+# Blenderに登録するクラスリスト
+classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_export_scene,
+    MYADDON_OT_add_filename,
+    MYADDON_OT_add_collider,
+    TOPBAR_MT_my_menu,
+    OBJECT_PT_file_name,
+    OBJECT_PT_collider,
+)
+
+# アドオン有効化時コールバック
+def register():
+    # Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    # メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    #3Dビューに描画関数を追加
+    DrawCollider.handle = bpy.types.SpaceView3D.draw_handler_add(DrawCollider.draw_collider, (), "WINDOW", "POST_VIEW")
+    print("レベルエディタが有効化されました。")
+
+# アドオン無効化時コールバック
+def unregister():
+    # メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    #3Dビューから描画関数を削除
+    bpy.types.SpaceView3D.draw_handler_remove(DrawCollider.handle, "WINDOW")
+
+    # Blenderからクラスを削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    print("レベルエディタが無効化されました。")
+    
+# テスト実行用コード
+if __name__ == "__main__":
+    register()
---
+++ stretch_vertex.py
@@ -0,0 +1,22 @@
+import bpy
+
+# オペレータ 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    # リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        # "Cube"オブジェクトが存在するか確認
+        if "Cube" in bpy.data.objects:
+            bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+            print("頂点を伸ばしました。")
+            self.report({'INFO'}, "頂点を伸ばしました。")
+        else:
+            self.report({'WARNING'}, "キューブが存在しません。")
+
+        # オペレータの命令終了を通知
+        return {'FINISHED'}
